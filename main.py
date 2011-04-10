90c9f02777532d3cf87e75561df2f0969800c6ec
Brian Staats <brian.staats@gmail.com>
2011-04-10 15:19:12 -0400

    Updated app to go thru main.py for index.html

--- main.py
+++ main.py
@@ -14,20 +14,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
+
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
+from google.appengine.ext.webapp import template
 
+from rescuetime.api.service import Service
 
 class MainHandler(webapp.RequestHandler):
-    def get(self):
-        self.response.out.write('Hello world!')
+  def get(self):
+    
+    s = Service()
+    
+    temp_values = {}
+    temp_file   = os.path.join(os.path.dirname(__file__), 'index.html')
+    self.response.out.write(template.render(temp_file, temp_values))
 
 
 def main():
-    application = webapp.WSGIApplication([('/', MainHandler)],
-                                         debug=True)
-    util.run_wsgi_app(application)
+  application = webapp.WSGIApplication([('/', MainHandler)],
+                                       debug=True)
+  util.run_wsgi_app(application)
 
 
 if __name__ == '__main__':
-    main()
+  main()
